a8a6fe977ed4147f885cbcc013a4b03c39c03c86
Guilherme Henrique Novaes de Souza <guilhermehenriquenovaes@gmail.com>
2021-09-29 20:30:33 -0300

    Criado novas instancias

--- src/hibernate/annotations/HibernateAnnotations.java
+++ src/hibernate/annotations/HibernateAnnotations.java
@@ -20,11 +20,13 @@ public class HibernateAnnotations {
     public static void main(String[] args) {
         Professor prf = new Professor("Guilherme", "guilherme@gmail.com", 1000);
         Disciplina disc = new Disciplina("Disciplina de matemática", "60hrs");
+        Professor prf02 = new Professor("Raphael", "raphael@gmail.com", 1200);
         
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
             session.save(prf);
             session.save(disc);
+            session.save(prf02);
             
         session.getTransaction().commit();
         HibernateUtil.shutdown();
